a295faf21ba71d3b8b0a41439588f0f1ac81b91a
Ken <kenjyco@gmail.com>
2018-12-19 07:51:54 -0600

    Bump to v0.1.6

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='vlc-helper',
-    version='0.1.5',
+    version='0.1.6',
     description='CLI helpers for VLC media player',
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/vlc-helper',
-    download_url='https://github.com/kenjyco/vlc-helper/tarball/v0.1.5',
+    download_url='https://github.com/kenjyco/vlc-helper/tarball/v0.1.6',
     packages=find_packages(),
     install_requires=[
         'chloop',
